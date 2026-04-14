a36463dfbfb5f6dedbe58df09f1125423987ed98
aretu-admin <ste.aretu@gmail.com>
2026-04-14 15:32:32 +0200

    write ascii data red function refactored

--- old_scripts/shivaUNIX.m
+++ old_scripts/shivaUNIX.m
@@ -1066,61 +1066,7 @@ function binary_Callback(hObject, eventdata, handles)
     'Write as',['~/',handles.filename]);
 cd (pat)
 
-
-for j=1:length(handles.column)
-    C(j,1)={'%10.6f '};
-    if j==length(handles.column)
-        C(j,1)={'%10.6f\n'};
-    end
-end
-C1=cell2mat(C');
-
-for j=1:length(handles.column)
-    N(j,1)={['handles.' handles.column{j} '(l,1),']};
-    if j==length(handles.column)
-        N(j,1)={['handles.' handles.column{j} '(l,1)']};
-    end
-end
-N1=cell2mat(N');
-
-
-for j=1:length(handles.column)
-    M(j,1)={['''' handles.column{j} '''' ',']};
-    if j==length(handles.column)
-        M(j,1)={['''' handles.column{j} '''']};
-    end
-end
-M1=cell2mat(M');
-
-%for j=1:length(handles.column)
-%    O(j,1)={['''v' num2str(j) '''' ',']};
-%    if j==length(handles.column)
-%         O(j,1)={['''v' num2str(j) '''']};
-%    end
-%end
-%O1=cell2mat(O');
-
-
-for j=1:length(handles.column)
-    S(j,1)={'%s '};
-    if j==length(handles.column)
-        S(j,1)={'%s\n'};
-    end
-end
-S1=cell2mat(S');
-
-%write in a file
-nome2=[nome, 'RED.txt'];
-fid = fopen(nome2,'wt');
-eval(['fprintf(fid,''' S1 ''',' M1 ');'])
-
-eval(['len=length(handles.' handles.column{1} ');'])
-
-for l=1:len
-    eval(['fprintf(fid,''' C1 ''',' N1 ');'])
-end
-fclose(fid);
-if ~ strcmp(fieldnames(handles),'dt'); msgbox(['ATTENTION: handles.dt=none']); end
+write_ascii_data_red(handles,nome)
 end
 
 %% SAVERED--------------------------------------------------------------------
