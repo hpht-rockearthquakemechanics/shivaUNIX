8423e4fc47371655b09abf3dabc8dc18c153c17f
aretu-admin <ste.aretu@gmail.com>
2026-04-14 15:24:09 +0200

    write ascii data function refactored

--- old_scripts/shivaUNIX.m
+++ old_scripts/shivaUNIX.m
@@ -139,74 +139,8 @@ function write_Callback(hObject, eventdata, handles)
     'Write as',['~/',handles.filename]);
 cd (pat)
 
+write_ascii_data(handles,nome)
 
-h_=findobj('Tag','fluid');
-statoF=get(h_,'Value');
-h_=findobj('Tag','GH');
-statoGH=get(h_,'Value');
-
-if statoF==1
-    I={'Time' 'shear1' 'EffPressure' 'Mu1' 'Pf' 'LVDT_low' 'LVDT_high' 'vel' 'slip' 'TempE' 'TempM'};
-elseif statoGH==1
-    I={'Time' 'shear1' 'Normal' 'Mu1' 'dspring' 'LVDT_low' 'vel' 'slip' 'TempE' 'TempM'};
-else
-    I={'Time' 'shear1' 'Normal' 'Mu1' 'LVDT_low' 'vel' 'slip' 'TempE'};
-end
-
-for j=1:length(I); %1:length(handles.column)
-    C(j,1)={'%10.6f '};
-    if j==length(I)
-        C(j,1)={'%10.6f\n'};
-    end
-end
-C1=cell2mat(C');
-
-for j=1:length(I); %1:length(handles.column)
-    N(j,1)={['handles.' I{j} '(l,1),']};
-    if j==length(I)
-        N(j,1)={['handles.' I{j} '(l,1)']};
-    end
-end
-N1=cell2mat(N');
-
-
-for j=1:length(I); %1:length(handles.column)
-    M(j,1)={['''' I{j} '''' ',']};
-    if j==length(I)
-        M(j,1)={['''' I{j} '''']};
-    end
-end
-M1=cell2mat(M');
-
-%for j=1:length(handles.column)
-%    O(j,1)={['''v' num2str(j) '''' ',']};
-%    if j==length(handles.column)
-%         O(j,1)={['''v' num2str(j) '''']};
-%    end
-%end
-%O1=cell2mat(O');
-
-
-for j=1:length(I); %1:length(handles.column)
-    S(j,1)={'%s '};
-    if j==length(I)
-        S(j,1)={'%s\n'};
-    end
-end
-S1=cell2mat(S');
-
-%write in a file
-nome2=[nome, 'RED.txt'];
-fid = fopen(nome2,'wt');
-eval(['fprintf(fid,''' S1 ''',' M1 ');'])
-
-eval(['len=length(handles.' handles.column{1} ');'])
-
-for l=1:len
-    eval(['fprintf(fid,''' C1 ''',' N1 ');'])
-end
-fclose(fid);
-if ~ strcmp(fieldnames(handles),'dt'); msgbox(['ATTENTION: handles.dt=none']); end
 end
 
 %% OPEN --------------------------------------------------------------------
